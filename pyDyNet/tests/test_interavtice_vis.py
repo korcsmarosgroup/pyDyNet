92052b16eac95a6b63678133450e0b7611046977
Luca <csabai.luca@gmail.com>
2021-10-08 14:12:45 +0200

    fix(file_path)[Luca]

--- pyDyNet/tests/test_interavtice_vis.py
+++ pyDyNet/tests/test_interavtice_vis.py
@@ -12,16 +12,16 @@ for filename in os.listdir("."):
 
 
 def test_convert_input_data():
-    node_df = pd.read_csv('pydynet_example_results/pydynet_example_results/node_dn_scores.tsv', sep='\t', index_col=0)
-    edge_df = pd.read_csv('pydynet_example_results/pydynet_example_results/reference_network_edge_list.tsv', sep='\t')
+    node_df = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/node_dn_scores.tsv', sep='\t', index_col=0)
+    edge_df = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/reference_network_edge_list.tsv', sep='\t')
     results = _convert_input_data(node_df, edge_df)
 
     assert list(results.nodes) == ['C', 'D', 'B', 'A', 'E']
 
 def test_draw_interactive_network():
 
-    node_df = pd.read_csv('pydynet_example_results/pydynet_example_results/node_dn_scores.tsv', sep='\t', index_col=0)
-    edge_df = pd.read_csv('pydynet_example_results/pydynet_example_results/reference_network_edge_list.tsv', sep='\t')
+    node_df = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/node_dn_scores.tsv', sep='\t', index_col=0)
+    edge_df = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/reference_network_edge_list.tsv', sep='\t')
     results = _convert_input_data(node_df, edge_df)
 
     html_results = _draw_interactive_network(results, 800, 800, "output")
