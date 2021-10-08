ba3b2c679651ebec6a064dfdec74789ba58d576b
Luca <csabai.luca@gmail.com>
2021-10-08 12:18:45 +0200

    add(interactive_caller)[Luca]

--- pyDyNet/visualization/interactive_vis.py
+++ pyDyNet/visualization/interactive_vis.py
@@ -3,43 +3,41 @@ from pyvis.network import Network
 import pandas as pd
 
 
-def convert_input_data(nodelist, edgelist):
+def convert_input_data(nodes, edges):
     """
     Assign colors to each node based on Dn-score
     :param nodelist: file containing nodes and Dn scores
     :param edgelist: file containing edges
     :return: networkx graph object
     """
-    nodes = pd.read_csv(nodelist)
-    edges = pd.read_csv(edgelist)
     # rescaling the rewiring values to RGB
     a, b = 255, 0
-    x = nodes['DyNet Rewiring (Dn-score)'].min()
-    y = nodes['DyNet Rewiring (Dn-score)'].max()
+    x = nodes['dn_score'].min()
+    y = nodes['dn_score'].max()
 
     # Save as separate columns
     nodes['R'] = 255
-    nodes['G'] = (nodes['DyNet Rewiring (Dn-score)'] - x) / (y - x) * (b - a) + a
+    nodes['G'] = (nodes['dn_score'] - x) / (y - x) * (b - a) + a
     nodes['G'] = nodes['G'].round().astype(int)
-    nodes['B'] = (nodes['DyNet Rewiring (Dn-score)'] - x) / (y - x) * (b - a) + a
+    nodes['B'] = (nodes['dn_score'] - x) / (y - x) * (b - a) + a
     nodes['B'] = nodes['B'].round().astype(int)
 
-    nodes = nodes[['Dn-Score (degree corrected)', 'DyNet Rewiring (Dn-score)', 'name', 'R', 'G', 'B']]
+    nodes = nodes[['dn_score', 'dn_score_corrected', 'n_edges', 'R', 'G', 'B']]
     # Maybe a python magician could rewrite this list comp to be more efficient
     nodes['color'] = ['#%02x%02x%02x' % (nodes['R'][i], nodes['G'][i], nodes['B'][i]) for i in range(len(nodes))]
 
     # rescaling the rewiring values to size
     c, d = 15, 50
-    nodes['value'] = (nodes['DyNet Rewiring (Dn-score)'] - x) / (y - x) * (d - c) + c
+    nodes['value'] = (nodes['dn_score'] - x) / (y - x) * (d - c) + c
     nodes['value'] = nodes['value'].round().astype(int)
 
     # Title controls hover behaviour in pyvis
-    nodes['title'] = ('name: ' + nodes['name'].astype('str')) + ' Dn: ' + (
-        nodes['DyNet Rewiring (Dn-score)'].astype('str'))
+    nodes['title'] = ('name: ' + nodes.index.astype('str')) + ' Dn: ' + (
+        nodes['dn_score'].astype('str'))
     # Create NetworkX object
 
     G = nx.from_pandas_edgelist(edges, 'source', 'target')
-    data = nodes.set_index('name').to_dict('index').items()
+    data = nodes.to_dict('index').items()
     G.add_nodes_from(data)
 
     return (G)
