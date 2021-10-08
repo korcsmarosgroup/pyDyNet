005651b91e9a1de60b8b8558726775367a79e30a
szabogtamas <szabogtamas@gmail.com>
2021-10-08 17:45:39 +0200

    Static wrapper (#28)
    * add (static caller) Added wrapper for static viz
    
    * add (wrapper test)
    
    * fix (test file path)
    
    * fix (separator problem in input)
    
    * fix (typo in colname)
    
    * change(expectation for retrun val of vis overview)
    
    * change (changed test dataset for overview)
    
    * fix (missing line in overview test)
    
    * fix (get figure from ax)

--- pyDyNet/tests/test_static_vis.py
+++ pyDyNet/tests/test_static_vis.py
@@ -1,9 +1,11 @@
 import os
-
+import matplotlib
 
 import pandas as pd
 import networkx as nx
+
 from ..visualization.static_vis import draw_static_network_overview
+from ..visualization.static_vis import vis_static
 
 for filename in os.listdir("."):
     
@@ -13,12 +15,25 @@ for filename in os.listdir("."):
 
 def test_draw_static_network_overview():
 
-    edges = pd.read_csv('pyDyNet/tests/vis_tests_files/edgetable.txt')
-    nodes = pd.read_csv('pyDyNet/tests/vis_tests_files/nodetable.txt')
+    edges = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/reference_network_edge_list.tsv', sep="\t")
+    nodes = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/node_dn_scores.tsv', sep="\t")
     G = nx.from_pandas_edgelist(edges, 'source', 'target')
-    G.add_nodes_from(nodes.set_index('name').to_dict('index').items())
+    G.add_nodes_from(nodes.set_index('Unnamed: 0').to_dict('index').items())
 
-    results = draw_static_network_overview(G, "Dn-Score (degree corrected)", main_title="An example plot of corrected scores")
+    results = draw_static_network_overview(G, main_title="An example plot of corrected scores")
+    result = results[0, 0]
+    
+    assert result.__class__.__name__ == 'AxesSubplot'
+
+
+def test_vis_static():
+
+    edges = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/reference_network_edge_list.tsv', sep="\t")
+    nodes = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/node_dn_scores.tsv', sep="\t")
+    G = nx.from_pandas_edgelist(edges, 'source', 'target')
+    G.add_nodes_from(nodes.set_index('Unnamed: 0').to_dict('index').items())
+
+    vis_static(G, "test_static.png")
     png_counter = 0
 
     for filename in os.listdir("."):
--- pyDyNet/visualization/static_vis.py
+++ pyDyNet/visualization/static_vis.py
@@ -1,27 +1,11 @@
-### Tools to be used
 import matplotlib
-from typing import Union, Callable
 from matplotlib import pyplot as plt
+
+from typing import Union, Callable
+
 import numpy as np
 import pandas as pd
 import networkx as nx
-
-
-### Start from an edge table
-
-#edges = pd.read_csv('../rewiring_test_data/edgetable.csv')
-#edges.head()
-
-### Rewiring scores are node features
-
-#nodes = pd.read_csv('../rewiring_test_data/nodetable.csv')
-#nodes.head()
-
-### Create network object as major expected input
-
-#G = nx.from_pandas_edgelist(edges, 'source', 'target')
-#G.add_nodes_from(nodes.set_index('name').to_dict('index').items())
-
 
 def draw_netstat_histo(
         values: list,
@@ -29,7 +13,7 @@ def draw_netstat_histo(
         ax: Union[None, plt.axes] = None,
 ) -> tuple:
     """
-    Draw a histogram of one of the visual chanels (color or size of nodes).
+    Draw a histogram of one of the visual channels (color or size of nodes).
 
     Parameters
     ----------
@@ -63,7 +47,7 @@ def draw_netstat_scatter(
         ax: Union[None, plt.axes] = None,
 ) -> tuple:
     """
-    Draw a histogram of one of the visual chanels (color or size of nodes).
+    Draw a histogram of one of the visual channels (color or size of nodes).
 
     Parameters
     ----------
@@ -93,144 +77,39 @@ def draw_netstat_scatter(
     return ax
 
 
-def check_node_feature_integrity(
-        network: nx.classes.graph.Graph,
-        node_list: list
-) -> tuple:
+def nx_node_features_to_pd(
+        network: nx.classes.graph.Graph
+) -> pd.DataFrame:
     """
-    Helper to check integrity between user supplied node list and the nodes
-    in the network.
+    Helper to convert node features into a dataframe for easier access.
 
     Parameters
     ----------
     network
-        The network that will be shown
-    node_list
-        Set of nodes to show; its order matters here
+        The network with node attributes
 
     Returns
     -------
-    The (filtered) network and the reconciled list of labels.
+    A table of node features.
     """
 
-    nodes_in_network = list(network)
+    node_features = pd.DataFrame.from_dict(network.nodes, orient="index").reset_index()
 
-    # As we are accepting arrays of values from outside the networkx object,
-    # implement some sort of filtering to avoid setting non-existent nodes
-    if node_list is None:
-        node_list = nodes_in_network
-    else:
-        node_list = pd.Series(node_list)
-        node_list = node_list.loc[node_list.isin(nodes_in_network)]
-        network = nx.subgraph(network, node_list)
-
-    return network, node_list
-
-
-def get_node_labels(
-        node_labels: Union[None, str, list, dict],
-        node_features: pd.DataFrame,
-        node_list: list
-) -> Union[None, dict]:
-    """
-    Helper to standardize user input of node labels.
-
-    Parameters
-    ----------
-    node_labels
-        User supplied node labels
-    node_features
-        Node features in tabular format for easier access (than the networkx format)
-    node_list
-        Set of nodes to show; its order matters here
-
-    Returns
-    -------
-    A mapping between node IDs and the labels to show.
-    """
-
-    # Node IDs might not be what we want to show, so check if user supplied custom values
-    if node_labels is not None:
-        if isinstance(node_labels, str):
-            node_labels = node_features.set_index("index").loc[:, node_labels].to_dict()
-        else:
-            if not isinstance(node_labels, dict):
-                node_labels = dict(zip(node_list, node_labels))
-    return node_labels
-
-
-def get_node_scores(
-        scores: Union[str, list],
-        node_features: pd.DataFrame,
-        node_list: list
-) -> Union[None, dict]:
-    """
-    Helper to standardize user input of node scores.
-
-    Parameters
-    ----------
-    scores
-        User supplied node weights
-    node_features
-        Node features in tabular format for easier access (than the networkx format)
-    node_list
-        Set of nodes to show; its order matters here
-
-    Returns
-    -------
-    A list of numerical values that will impact on node colors.
-    """
-
-    if isinstance(scores, str):
-        scores = node_features[scores]
-    else:
-        if isinstance(scores, dict):
-            scores = [scores[x] for x in node_list]
-
-    return scores
-
-
-def get_node_sizes(
-        node_sizes: Union[None, str, list, dict],
-        node_features: pd.DataFrame,
-        node_list: list
-) -> Union[None, dict]:
-    """
-    Helper to standardize user input of node size weights.
-
-    Parameters
-    ----------
-    node_sizes
-        User supplied value if node size correspond to something else than degree
-    node_features
-        Node features in tabular format for easier access (than the networkx format)
-    node_list
-        Set of nodes to show; its order matters here
-
-    Returns
-    -------
-    A list of numerical values that will impact on node colors.
-    """
-
-    if isinstance(node_sizes, str):
-        node_sizes = node_features[node_sizes]
-    else:
-        if isinstance(node_sizes, dict):
-            node_sizes = [node_sizes[x] for x in node_list]
-
-    return node_sizes
+    return node_features
 
 
 def draw_static_network(
         network: nx.classes.graph.Graph,
-        scores: Union[str, list],
+        node_features: Union[None, pd.DataFrame] = None,
+        scores: str = "dn_score",
+        node_sizes: str = "n_edges",
         node_list: Union[None, list] = None,
-        node_sizes: Union[None, str, list, dict] = None,
-        node_labels: Union[None, str, list, dict] = None,
+        node_labels: Union[None, str, dict] = None,
         node_positions: Union[None, dict] = None,
         node_size_factor: Union[int, float, Callable] = 50,
         node_colormap: matplotlib.colors.LinearSegmentedColormap = plt.cm.Reds,
         color_min_max: Union[None, tuple] = None,
+        colorbar: bool = True,
         graph_title: str = "",
         ax: Union[None, plt.axes] = None,
         **networkx_kwds
@@ -242,63 +121,65 @@ def draw_static_network(
     ----------
     network
         A networkx object with scores added ad node features.
+    node_features
+        If node features are supplied, we can save time converting the network to pandas.
     scores
-        The scores that will determine node colors. If a single string is given, then it is
-        assumed to be the name of the attribute; if a list than those numbers in the list
-        (not recommended as it must be same order as nodelist or mapping is not guarantied)
+        The scores that will determine node colors. Has to be a column name in node features.
     node_list
-        List of nodes to show; useful if we want to prefilter based on score or pathway
+        Set of nodes to show; its order matters here
     node_sizes
-        A list, or even better, dictionary of node sizes; using degree if not specified
+        Name of node attribute (e.g. number of edges) that will correspond to node size.
     node_labels
-        A mapping of node labels if it is not identical to the node index
+        A mapping of node labels if it is not identical to the node index.
     node_positions
-        A mapping of node positions; fixing positions between samples can help comparison
+        A mapping of node positions; fixing positions between samples can help comparison.
     node_size_factor
-        A multiplier that is applied to the node sizes over the score values
+        A multiplier that is applied to the node sizes over the score values.
     node_colormap
-        A colormap that will be used to show scores
+        A colormap that will be used to show scores.
     color_min_max
         By default, the color scale spans from minimum score to maximum. Provide a tuple of
-        min and max values to override this
+        min and max values to override this.
+    colorbar
+        If a colorbar should be added.
     graph_title
-        A title put on the plot
+        A title put on the plot.
     ax
-        The pyplot axis if the network should be drawn on a predefined axis
+        The pyplot axis if the network should be drawn on a predefined axis.
     networkx_kwds
-        A dictionary of keywords passed over to `draw_networkx`
+        A dictionary of keywords passed over to `draw_networkx`.
 
     Returns
     -------
     A matplotlib axis where the network was drawn.
     """
 
-    # As visualisation heavily relies on node features and node order, try to reconsiliate node list
-    network, node_list = check_node_feature_integrity(network, node_list)
-
-    # We will use node features a lot, so easiest is to retreive them as a dataframe
-    node_features = pd.DataFrame.from_dict(network.nodes, orient="index").reset_index()
-
-    # Node IDs might not be what we want to show, so check if user supplied custom values
-    node_labels = get_node_labels(node_labels, node_features, node_list)
-
-    # Scores can be supplied by the user in various ways, so convert it to a list of right order
-    scores = get_node_scores(scores, node_features, node_list)
-
-    # Just like scores, sizes might also be specified upstream, so check
-    if node_sizes is None:
-        node_sizes = dict(network.degree)
-    node_sizes = get_node_sizes(node_sizes, node_features, node_list)
-
-    # Usual case would be to supply a linear scaling factor for node sizes, but it is also possible to scale non-linearly
-    if isinstance(node_size_factor, (int, float)):
-        node_sizes = np.array(node_sizes) * node_size_factor
+    # Keep a possibility to supply a node_list by which the big network will be filtered
+    if node_list is not None:
+        nodes_in_network = list(network)
+        node_list = pd.Series(node_list)
+        node_list = node_list.loc[node_list.isin(nodes_in_network)]
+        network = nx.subgraph(network, node_list)
+        if node_features is None:
+            node_features = nx_node_features_to_pd(network)
+        else:
+            nodes_in_network = list(network)
+            node_features = node_features.loc[nodes_in_network]
     else:
-        node_sizes = node_size_factor(node_sizes)
+        if node_features is None:
+            node_features = nx_node_features_to_pd(network)
 
     # For better comparison, fixed node positions might be supplied
     if node_positions is None:
         node_positions = nx.spring_layout(network, k=0.08)
+
+    # Usual case would be to supply a linear scaling factor for node sizes, but it is also possible to scale non-linearly
+    if isinstance(node_size_factor, (int, float)):
+        node_sizes = np.array(node_features[node_sizes]) * node_size_factor
+    else:
+        node_sizes = node_size_factor(node_features[node_sizes])
+    
+    scores = node_features[scores]
 
     # Check if user supplied colorscale limits
     if color_min_max is None:
@@ -317,19 +198,23 @@ def draw_static_network(
     # In most cases, the function should draw on an already existing axis, but let it handle missing case
     if ax is None:
         fig, ax = plt.subplots()
-    ax.set_title(graph_title, y=0.9)
+    else:
+        fig = ax.get_figure()
+    ax.set_title(graph_title)
 
     # Finally, draw the network graph
     nx.draw_networkx(
-        network, pos=node_positions, node_color=scores, node_size=node_sizes, labels=node_labels,
-        edge_color="silver", cmap=node_colormap, vmin=vmin, vmax=vmax, ax=ax, **networkx_kwds
+        network, pos=node_positions, node_color=scores, node_size=node_sizes,
+        labels=node_labels, edge_color="silver", cmap=node_colormap,
+        vmin=vmin, vmax=vmax, ax=ax, **networkx_kwds
     )
 
-    # Add a colorbar to the axis
-    sm = plt.cm.ScalarMappable(cmap=node_colormap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
-    sm.set_array([])
-    cbar = plt.colorbar(sm)
-
+    # Add a colorbar to the axis if needed
+    if colorbar:
+        sm = plt.cm.ScalarMappable(cmap=node_colormap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
+        sm.set_array([])
+        cbar = fig.colorbar(sm)
+    
     # Remove spines
     ax.axis('off')
 
@@ -338,10 +223,10 @@ def draw_static_network(
 
 def draw_static_network_overview(
         network: nx.classes.graph.Graph,
-        scores: Union[str, list],
-        node_list: Union[None, list] = None,
-        node_sizes: Union[None, str, list, dict] = None,
-        node_labels: Union[None, str, list, dict] = None,
+        node_features: Union[None, pd.DataFrame] = None,
+        scores: str = "dn_score",
+        node_sizes: str = "n_edges",
+        node_labels: Union[None, str, dict] = None,
         node_positions: Union[None, dict] = None,
         node_size_factor: Union[int, float, Callable] = 50,
         node_colormap: matplotlib.colors.LinearSegmentedColormap = plt.cm.Reds,
@@ -359,33 +244,41 @@ def draw_static_network_overview(
     ----------
     network
         A networkx object with scores added ad node features.
-    score_column
-        Name of the feature to show on this figure
-    node_list
-        List of nodes to show; useful if we want to prefilter based on score or pathway
+    node_features
+        If node features are supplied, we can save time converting the network to pandas.
+    scores
+        The scores that will determine node colors. Has to be a column name in node features.
+    node_sizes
+        Name of node attribute (e.g. number of edges) that will correspond to node size.
+    node_labels
+        A mapping of node labels if it is not identical to the node index.
     node_positions
-        A mapping of node positions; fixing positions between samples can help comparison
+        A mapping of node positions; fixing positions between samples can help comparison.
+    node_size_factor
+        A multiplier that is applied to the node sizes over the score values.
+    node_colormap
+        A colormap that will be used to show scores.
+    color_min_max
+        By default, the color scale spans from minimum score to maximum. Provide a tuple of
+        min and max values to override this.
+    main_title
+        Title to be put above the whole panel.
     show_stats
-        If the two extra plots should also be shown or just the network
+        If the two extra plots should also be shown or just the network.
     figsize
-        Figure size; just to aim big, it defaults to a full-page A4 figure
+        Figure size; just to aim big, it defaults to a full-page A4 figure.
 
     Returns
     -------
     A tuple of matplotlib axes.
     """
 
+    node_list = None
+    if node_features is None:
+        node_features = nx_node_features_to_pd(network)
+
     # If some descriptive stats for scores and degrees are to be shown, it makes sense to access them now
     if show_stats:
-
-        # Reconcile client inputs just as the static network drawer would do it, just a bit earlier
-        network, node_list = check_node_feature_integrity(network, node_list)
-        node_features = pd.DataFrame.from_dict(network.nodes, orient="index").reset_index()
-        node_labels = get_node_labels(node_labels, node_features, node_list)
-        scores = get_node_scores(scores, node_features, node_list)
-        if node_sizes is None:
-            node_sizes = dict(network.degree)
-        node_sizes = get_node_sizes(node_sizes, node_features, node_list)
 
         fig, axs = plt.subplots(
             2, 3, figsize=figsize,
@@ -397,23 +290,70 @@ def draw_static_network_overview(
         ax_net = fig.add_subplot(gs[0, 0:])
 
         draw_static_network(
-            network, scores, node_list, node_sizes, node_labels, node_positions,
-            node_size_factor, node_colormap, color_min_max, ax=ax_net
+            network, node_features, scores, node_sizes, node_labels, node_positions,
+            node_list, node_size_factor, node_colormap, color_min_max, ax=ax_net
         )
 
-        draw_netstat_histo(scores, label="Color", ax=axs[1, 0])
-        draw_netstat_scatter(scores, node_sizes, labels=("Color", "Size"), ax=axs[1, 1])
-        draw_netstat_histo(node_sizes, label="Size", ax=axs[1, 2])
+        scores_l = node_features[scores]
+        node_sizes_l = node_features[node_sizes]
 
-        # If stats plots are not required, we can leave everything to the static network drawer
+        draw_netstat_histo(scores_l, label="Color", ax=axs[1, 0])
+        draw_netstat_scatter(scores_l, node_sizes_l, labels=("Color", "Size"), ax=axs[1, 1])
+        draw_netstat_histo(node_sizes_l, label="Size", ax=axs[1, 2])
+
+    # If stats plots are not required, we can leave everything to the static network drawer
     else:
         fig, axs = plt.subplots(figsize=figsize)
         draw_static_network(
-            network, scores, node_list, node_sizes, node_labels, node_positions,
-            node_size_factor, node_colormap, color_min_max, ax=axs
+            network, node_features, scores, node_sizes, node_labels, node_positions,
+            node_list, node_size_factor, node_colormap, color_min_max, ax=axs
         )
 
     fig.suptitle(main_title)
-    plt.savefig("static_net.png")
 
     return axs
+
+
+def vis_static(
+        network: nx.classes.graph.Graph,
+        out_filename: str = "output/static_graph_overview.png",
+        scores: str = "dn_score",
+        node_sizes: str = "n_edges",
+        main_title: str = "",
+        show_stats: bool = True,
+        figsize: tuple = (7.2, 9.6)
+) -> None:
+    """
+    High-level function to draw an overview of the scored network and save the output.
+
+    Parameters
+    ----------
+    network
+        A networkx object with scores added ad node features.
+    out_filename
+        Path where figure needs to be saved.
+    scores
+        The scores that will determine node colors. Has to be a column name in node features.
+    node_sizes
+        Name of node attribute (e.g. number of edges) that will correspond to node size.
+    main_title
+        Title to be put above the whole panel.
+    show_stats
+        If the two extra plots should also be shown or just the network.
+    figsize
+        Figure size; just to aim big, it defaults to a full-page A4 figure.
+
+    Returns
+    -------
+    Does not return anything (an IO stream later?)
+    """
+
+    axs = draw_static_network_overview(
+        network=network, scores=scores, node_sizes=node_sizes, main_title=main_title,
+        show_stats=show_stats, figsize=figsize
+     )
+    
+    fig = axs.flatten()[-1].get_figure()
+    fig.savefig(out_filename)
+
+    return
