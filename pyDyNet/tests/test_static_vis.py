da16593107b5ff9d98178497c3570d84b6a5ad4f
szabogtamas <szabogtamas@gmail.com>
2021-10-08 20:28:59 +0200

    add (facet plot) Added network facet vis

--- pyDyNet/tests/test_static_vis.py
+++ pyDyNet/tests/test_static_vis.py
@@ -1,9 +1,10 @@
 import os
-import matplotlib
+import pickle
 
 import pandas as pd
 import networkx as nx
 
+from ..visualization.static_vis import draw_subnetwork_facets
 from ..visualization.static_vis import draw_static_network_overview
 from ..visualization.static_vis import vis_static
 
@@ -11,6 +12,26 @@ for filename in os.listdir("."):
     
     if filename.endswith(".png"):
         os.remove(filename)
+
+
+def draw_subnetwork_facets():
+
+    edges = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/reference_network_edge_list.tsv', sep="\t")
+    nodes = pd.read_csv('pyDyNet/tests/pydynet_example_results/pydynet_example_results/node_dn_scores.tsv', sep="\t")
+    G = nx.from_pandas_edgelist(edges, 'source', 'target')
+    G.add_nodes_from(nodes.set_index('Unnamed: 0').to_dict('index').items())
+
+    top_nodes = nodes.sort_values(by="dn_score").tail(2)
+    top_nodes = top_nodes.index.tolist()
+
+    with open('../pydynet_example_results/pydynet_nx_network_list.pkl','rb') as infile:
+        input_graph_collection = pickle.load(infile)
+        input_graph_names = ["Graph_" + str(x) for x in range(1, len(input_graph_collection)+1)]
+
+    results = draw_subnetwork_facets(G, input_graph_collection, input_graph_names, nodelist=top_nodes)
+    result = results[0, 0]
+    
+    assert result.__class__.__name__ == 'AxesSubplot'
 
 
 def test_draw_static_network_overview():
@@ -33,7 +54,7 @@ def test_vis_static():
     G = nx.from_pandas_edgelist(edges, 'source', 'target')
     G.add_nodes_from(nodes.set_index('Unnamed: 0').to_dict('index').items())
 
-    vis_static(G, "test_static.png")
+    vis_static(G, "vis_static_test.png")
     png_counter = 0
 
     for filename in os.listdir("."):
